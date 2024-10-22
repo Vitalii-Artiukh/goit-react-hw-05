f6eb15179a9cd0f9d96a255789babe40c4a18cf0
Vitalii Artiukh <radial_car@yahoo.com>
2024-10-23 01:23:09 +0300

    SearchMovies

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,10 +1,10 @@
-import { React, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { fetchMoviesSearch } from '../components/API/ApiRequwests';
 import MovieList from '../components/MovieList/MovieList';
 import Loader from '../components/Loader/Loader';
 import ErrorMessage from '../components/ErrorMessage/ErrorMessage';
-import SearchMovies from '../components/searchMovies/SearchMovies';
+import SearchMovies from '../components/SearchMovies/SearchMovies';
 
 const MoviesPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
